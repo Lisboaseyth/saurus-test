9be2cadab9c61c0e2e0f0e7178a9a467c1a7b2e2
Pedro Lisboa <pedrolisboadeveloper@gmail.com>
2025-07-20 10:35:30 -0300

    feat: structure for project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Stack, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Stack>
+      <Text>Teste</Text>
+    </Stack>
+  );
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    primary: "#2B6CB0",
+    secondary: "#2C5282",
+  },
+});
+
+export default theme;
